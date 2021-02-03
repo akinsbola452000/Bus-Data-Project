e4ec0af2454d662e6abfbe0906c5437be6591641
Bolarinwa Akindaini <bolarinwa.akindaini@happy-or-not.com>
2021-02-03 13:47:28 +0200

    Updating code to python 3.7 format

--- busdata.py
+++ busdata.py
@@ -2,7 +2,7 @@ import urllib.request
 import sys
 import time
 
-sleep = int(sys.argv[2])
+sleep = 5 #int(sys.argv[2])
 timespan = int(sys.argv[3])
 end = time.time() + timespan
 i = 0
@@ -10,6 +10,6 @@ with open(sys.argv[1], 'w') as resfile:
     while time.time() < end:
         i += 1
         print >> sys.stderr, 'Iteration', str(i)
-        data = urllib.urlopen("http://data.itsfactory.fi/journeys/api/1/vehicle-activity")
+        data = urllib.request.urlopen("http://data.itsfactory.fi/journeys/api/1/vehicle-activity")
         print >> resfile, data.read()
         time.sleep(sleep)
